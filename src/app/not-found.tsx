16c82aa2c4fdcf2a63cf153b8dbfda82e5e69c2e
SUPINKIM <qorgkr26@sookmyung.ac.kr>
2024-10-19 19:36:33 +0900

    fix: 404 image webp 확장자로 변환

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,14 @@ import Image from "next/image";
 export default function Custom404() {
   return (
     <div className="w-full h-screen flex items-center flex-col flex-wrap justify-center">
-      <Image src={"/not-found.png"} height={240} width={360} alt="not-found" />
+      <Image
+        src={"/not-found.webp"}
+        priority
+        height={300}
+        width={320}
+        sizes="auto"
+        alt="not-found"
+      />
 
       <h1 className="font-semibold mt-[24px] text-[16px]">
         요청하신 페이지를 찾을 수 없습니다.
