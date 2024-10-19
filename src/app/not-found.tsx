3bafd2a1e796b78e530c064ea05e3f330e739db6
SUPINKIM <qorgkr26@sookmyung.ac.kr>
2024-10-19 18:31:02 +0900

    feat: 404page 커스텀

---
+++ src/app/not-found.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Custom404() {
+  return (
+    <div className="w-full h-screen flex items-center flex-col flex-wrap justify-center">
+      <Image src={"/not-found.png"} height={240} width={360} alt="not-found" />
+
+      <h1 className="font-semibold mt-[24px] text-[16px]">
+        요청하신 페이지를 찾을 수 없습니다.
+      </h1>
+      <h2>주소가 맞는지 다시 한 번 확인 부탁드려요!</h2>
+    </div>
+  );
+}
